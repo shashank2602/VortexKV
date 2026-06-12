shashank2602/VortexKV

commit 0ef642ae075d6681930389863c0bc9a5aea6dfcf

Fix data correctness bug in flatmap resize step.

inc/storage/flatMap.h
@@ -442,7 +442,7 @@ private:
 
 	void resizeStep() {
 		uint32_t steps = m_currentResizeStep;
-		while (steps-- > 0 && m_migrateIndex < m_tableOld.size)
+		while (m_migrateIndex < m_tableOld.size && (steps-- > 0 || m_tableOld.metadata[m_migrateIndex].psl != 0))
 		{
 			if (m_tableOld.metadata[m_migrateIndex].psl != 0)
 			{

commit aa2a8ada02f716fbf5f0053ce257e6f4b218ecc9

Commit 0ef642ae introduced a new bug which could cause entire table migration in one go because of signed integer overflow

inc/storage/flatMap.h
@@ -442,7 +442,7 @@ private:
 
 	void resizeStep() {
 		uint32_t steps = m_currentResizeStep;
-		while (m_migrateIndex < m_tableOld.size && (steps-- > 0 || m_tableOld.metadata[m_migrateIndex].psl != 0))
+		while (m_migrateIndex < m_tableOld.size && (steps > 0 || m_tableOld.metadata[m_migrateIndex].psl != 0))
 		{
 			if (m_tableOld.metadata[m_migrateIndex].psl != 0)
 			{
@@ -457,6 +457,8 @@ private:
 				}
 			}
 			m_migrateIndex++;
+			if(steps > 0)
+				steps--;
 		}
 
 		if (m_migrateIndex >= m_tableOld.size)